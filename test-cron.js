675a91742d911c31684cf61c01814ac4f78d2391
Dean Sallinen <deza604@gmail.com>
2017-10-11 05:26:53 +0000

    remove all test files

--- test-cron.js
+++
@@ -1,8 +0,0 @@
-var CronJob = require("cron").CronJob;
-
-var test = require("./scraper.js");
-
-new CronJob('1 * * * * *', function() {
-    test.scrape();
-    console.log('Message every minute');
-}, null, true, 'America/Vancouver');
