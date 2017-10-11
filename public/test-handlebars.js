ac834bef434a84a3422b54924320c784e787f46f
Dean Sallinen <deza604@gmail.com>
2017-10-11 05:52:47 +0000

    fix package start scripts

--- public/test-handlebars.js
+++
@@ -1,7 +0,0 @@
-var theData = {headerTitle:"Shop Page", weekDay:"Wednesday"};
-// Retrieve the HTML from the script tag we setup in step 1
-// We use the id (header) of the script tag to target it on the page​
-var theTemplateScript = $("#header").html();
-// The Handlebars.compile function returns a function to theTemplate variable
-var theTemplate = Handlebars.compile (theTemplateScript);
-$(document.body).append (theTemplate (theData));
