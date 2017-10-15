75b9dca3dc0f2d226fd753226c0d4e52da228e9e
Dean Sallinen <deza604@gmail.com>
2017-10-15 19:29:09 +0000

    add slide animation to cards

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
     $(".accordion").click(function() {
-        $(this).toggleClass("active");
-        $(this).next(".panel").toggleClass("active");
+        // $(this).toggleClass("active");
+        // $(this).next(".panel").toggleClass("active");
         // $(this).children("a").attr("href", "#");
+    });
+
+    $("h3.routeName").click(function() {
+        $(this).parent().toggleClass("active");
+        $(this).next().slideToggle("fast");
     });
     // $("h2:contains('Tsawwassen')").css("background", "green");
 });
