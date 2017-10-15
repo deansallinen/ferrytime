7e181465e1228586ef833bedbdd065088479c19f
Dean Sallinen <deza604@gmail.com>
2017-10-15 21:13:26 +0000

    add readme

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $(".accordion").click(function() {
-        // $(this).toggleClass("active");
-        // $(this).next(".panel").toggleClass("active");
-        // $(this).children("a").attr("href", "#");
-    });
+    // $(".accordion").click(function() {
+    //     // $(this).toggleClass("active");
+    //     // $(this).next(".panel").toggleClass("active");
+    //     // $(this).children("a").attr("href", "#");
+    // });
 
     $("h3.routeName").click(function() {
         $(this).parent().toggleClass("active");
---
+++ readme.md
@@ -0,0 +1,13 @@
+Ferry Tracker
+=============
+
+A simple website to track the schedule of the BC Ferries.
+
+# TODO
+
+- Separate Departure and Arrival terminals in header.
+- Right align schedule times
+- Add "last updated" datetime
+    - Check update frequency
+- Sailing indicators
+- Expanding sailings
