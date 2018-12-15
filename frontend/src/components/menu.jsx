57bc3fa3be0ba44f7266bdf8484bcf2567dae61a
Dean Sallinen <deza604@gmail.com>
2018-12-14 20:34:30 -0800

    fix: spacing on favourites on homepage

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StaticQuery, graphql, Link } from 'gatsby';
 import {
-  Ancestor, Parent, Child, Columns, Column, Container, H1,
+  Ancestor, Parent, Child, Columns, Column, Container, H1, Section,
 } from './helpers';
 
 export default () => {
@@ -24,57 +24,31 @@ export default () => {
       }
     `}
     render={data => (
-      <Container>
-        {favourites.length
-          ? (
-            <div>
-              <H1>Favourites</H1>
-              <Columns>
-                <Ancestor>
-                  <Column>
-                    {data.allSitePage.edges
-                      .filter(each => /^\/route/.test(each.node.path))
-                      .filter(each => favourites.includes(each.node.context.routeName))
-                      .map(each => (
-                        <Parent>
-                          <Link to={each.node.path} key={each.node.context.routeId}>
-                            <Child className="box">
-                              <h5 className="title is-5">
+      <Section>
+        <Container>
+          <H1>All Routes</H1>
+          <Columns>
+            <Ancestor>
+              <Column>
+                {data.allSitePage.edges
+                  .filter(each => /^\/route/.test(each.node.path))
+                  .map(each => (
+                    <Parent>
+                      <Link to={each.node.path} key={each.node.context.routeId}>
+                        <Child className="box">
+                          <h5 className="title is-5">
 
-                                {each.node.context.routeName}
-                              </h5>
-                            </Child>
-                          </Link>
-                        </Parent>
-                      ))}
-                  </Column>
-                </Ancestor>
-              </Columns>
-            </div>
-          )
-          : null}
-        <H1>All Routes</H1>
-        <Columns>
-          <Ancestor>
-            <Column>
-              {data.allSitePage.edges
-                .filter(each => /^\/route/.test(each.node.path))
-                .map(each => (
-                  <Parent>
-                    <Link to={each.node.path} key={each.node.context.routeId}>
-                      <Child className="box">
-                        <h5 className="title is-5">
-
-                          {each.node.context.routeName}
-                        </h5>
-                      </Child>
-                    </Link>
-                  </Parent>
-                ))}
-            </Column>
-          </Ancestor>
-        </Columns>
-      </Container>
+                            {each.node.context.routeName}
+                          </h5>
+                        </Child>
+                      </Link>
+                    </Parent>
+                  ))}
+              </Column>
+            </Ancestor>
+          </Columns>
+        </Container>
+      </Section>
     )}
   />
   );
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Menu from '../components/menu';
+import Favourites from '../components/favourites';
 import { H1, Section, Container } from '../components/helpers';
 
 
 const IndexPage = () => (
   <Layout>
-    <Section>
-      <Menu />
-    </Section>
+    <Favourites />
+    <Menu />
   </Layout>
 );
 export default IndexPage;
