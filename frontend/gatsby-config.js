8aaa7de91d5d39dd276de14f7519f28384c58cf4
Dean Sallinen <deza604@gmail.com>
2018-12-21 09:58:45 -0800

    add analytics code

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -38,5 +38,25 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-51101141-2',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'ferryti.me',
+      },
+    },
   ],
 };
