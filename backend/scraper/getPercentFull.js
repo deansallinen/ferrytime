6821d6a2e2a6bbc2d52e6fbdc0aaef4d95382bd6
Dean Sallinen <deza604@gmail.com>
2019-03-15 18:44:26 -0700

    refactor scraper and finish scraping logic for parking full

---
+++ backend/scraper/getPercentFull.js
@@ -0,0 +1,49 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const moment = require('moment-timezone');
+
+const getPercentFull = ({
+  scheduled_departure,
+  departure_term,
+  route_num_str,
+}) => {
+  const inputTime = moment(scheduled_departure).format('HHmm');
+
+  const options = {
+    uri: `https://orca.bcferries.com/cc/marqui/sailingDetail.asp?route=${route_num_str}&dept=${departure_term}&ID=${inputTime}&src=ltr`,
+    transform: body => cheerio.load(body),
+  };
+
+  return rp(options).then($ => {
+    const sailingTime = $('.white-header-bold-lg')
+      .text()
+      .match(/\d+:\d+\s[AP]M/);
+
+    if (!sailingTime) return null;
+
+    const outputTime = moment(sailingTime[0], 'hh:mm aa').format('HHmm');
+
+    if (inputTime !== outputTime) return null;
+
+    const [percent_full] = $(
+      'table tbody tr td table tbody tr td form table tbody tr td div table tbody tr td table'
+    )
+      .attr('width')
+      .match(/\d{1,2}/);
+
+    const [parking_full] = $('.ccInfoTitle')
+      .parent()
+      .text()
+      .match(/\d{1,3}/);
+
+    const [car_waits, oversize_waits] = $('#quicklink-box tr td div')
+      .text()
+      .match(/(\d+)/);
+
+    return { percent_full, parking_full, car_waits, oversize_waits };
+  });
+};
+
+// getPercentFull(inputs).then(res => console.log( res))
+
+module.exports = { getPercentFull };
--- backend/scraper/index.js
+++ backend/scraper/index.js
@@ -2,232 +2,113 @@ require('dotenv').config();
 
 const scraper = require('table-scraper');
 const { request } = require('graphql-request');
-const moment = require('moment-timezone');
-const { flatten, merge } = require('lodash');
 
-const { upsertRoute, upsertSailing, upsertSailingPercent } = require('../queries');
+const { getPercentFull } = require('./getPercentFull.js');
+const { getSailingsArray } = require('./getSailingsArray.js');
 
-const {getPercentFull} = require('./cheerio.js')
+const {
+  upsertRoute,
+  upsertSailing,
+  upsertSailingPercent,
+  upsertParkingAndWaits,
+} = require('../queries');
 
 const uri = process.env.ENDPOINT;
 
-const makeRouteInfo = array => ({
-  route_name: array[0][0].split('Sailing time: ')[0],
-  average_sailing: array[0][0].split('Sailing time: ')[1],
-  sailing_date: new Date(array[0][1]).toISOString().substr(0, 10)
-});
-
-const validateTime = time => {
-  const isTime = /\d+:\d\d[AP]M/i.test(time.replace(/\s/g, ''));
-  return isTime;
-};
-
-const getHourAndMinute = time =>
-  moment(time, 'hh:mm a')
-    .format('HH:mm')
-    .split(':');
-
-const getUTCTime = rawTime => {
-  const isTime = validateTime(rawTime);
-  if (!isTime) return null;
-  const [hour, minute] = getHourAndMinute(rawTime);
-  const dateTime = moment
-    .tz('America/Vancouver')
-    .hour(hour)
-    .minute(minute)
-    .second(0);
-  // console.log(
-  //   rawTime,
-  //   hour,
-  //   minute,
-  //   dateTime.format(),
-  //   dateTime.utc().format()
-  // );
-  return dateTime.utc().format();
-};
-
-const makeSailing = object => {
-  const [
-    vessel,
-    scheduled_departure_time,
-    actual_departure_time,
-    eta_time,
-    sailing_status
-  ] = Object.values(object);
-  const scheduled_departure = getUTCTime(scheduled_departure_time);
-  const actual_departure = getUTCTime(actual_departure_time);
-  const eta = getUTCTime(eta_time);
-  // const percent_full = null;
-  return {
-    vessel,
-    sailing_status,
-    scheduled_departure,
-    actual_departure,
-    eta
-    // percent_full
-  };
-};
-
-const objectifySailingArray = array =>
-  array.reduce(
-    (obj, sailing) => ((obj[sailing.scheduled_departure] = sailing), obj),
-    {}
-  );
-
-const compileSailings = rawSchedule => {
-  rawSchedule.shift();
-  const sailingsArray = rawSchedule.map(x => makeSailing(x));
-  const sailingsObject = objectifySailingArray(sailingsArray);
-  return sailingsObject;
-};
-
-const getSailingsArray = data => {
-  const l = data.length;
-  const routesArray = [];
-  for (let i = 2; i < l; i += 2) {
-    const route = makeRouteInfo(data[i]);
-    route.sailings = compileSailings(data[i + 1]);
-    routesArray.push(route);
-  }
-  return routesArray;
-};
-
-const scrapeConditionsPage = async () =>
-  scraper.get('https://orca.bcferries.com/cc/marqui/at-a-glance.asp');
-
-const scrapeSailingsPage = async () =>
-  scraper.get('http://orca.bcferries.com:8080/cc/marqui/actualDepartures.asp');
-
-const getRouteTables = pageData =>
-  pageData.filter(each => each[0][0] === 'Route');
-
-const splitTimeFromPercent = input => {
-  const regex = /(\d{1,2}:\d\d[ap]m)(\d+)%/g;
-  const res = regex.exec(input);
-  if (!res) return null;
-  const [_, time, percentage] = res;
-  const scheduled_departure = getUTCTime(time);
-  return {
-    scheduled_departure,
-    percent_full: parseInt(percentage)
-  };
-};
-
-const getSailingsObject = percentString => {
-  if (!/^\d/.test(percentString)) return {};
-  const percentArray = percentString
-    .split(' full')
-    .filter(Boolean)
-    .map(splitTimeFromPercent);
-  const percentObject = objectifySailingArray(percentArray);
-  return percentObject;
-};
-
-const createRouteObject = route => ({
-  route_name: route[0],
-  // sailings: getSailingsObject(route[1]),
-  car_waits: parseInt(route[route.length - 3]),
-  oversize_waits: parseInt(route[route.length - 2])
-});
-
-const getConditionsArray = pageData => {
-  const routeTables = getRouteTables(pageData);
-  const removeBlankValues = row => Object.values(row).filter(Boolean);
-  const removeBlankRows = rows =>
-    rows.filter(element => Object.keys(element).length > 3 && element[0]);
-  return flatten(
-    routeTables.map(routeTable => {
-      const [headers, ...rows] = routeTable;
-      return removeBlankRows(rows).map(row => {
-        const route = removeBlankValues(row);
-        return createRouteObject(route);
-      });
-    })
-  );
-};
-
-const sort = routeArray =>
-  routeArray.sort((a, b) => {
-    const routeA = a.route_name.toUpperCase();
-    const routeB = b.route_name.toUpperCase();
-    if (routeA < routeB) return -1;
-    if (routeA > routeB) return 1;
-    return 0;
-  });
-
-const scrape = async () => {
+const scrapeSailingSchedule = async () => {
   console.log('\nScraping sailings schedule...');
-  const sailingPage = await scrapeSailingsPage();
-  const conditionsPage = await scrapeConditionsPage();
+  const sailingPage = await scraper.get(
+    'http://orca.bcferries.com:8080/cc/marqui/actualDepartures.asp'
+  );
 
   const sailings = getSailingsArray(sailingPage);
-  const conditions = getConditionsArray(conditionsPage);
-  // console.log(JSON.stringify(sailings, null, 2))
-  // console.log(JSON.stringify(conditions, null, 2))
 
-  const result = merge(sort(sailings), sort(conditions));
-  // console.log(JSON.stringify(result, null, 2));
-
-  console.log(`Scraped ${result.length} routes`);
-  return result;
+  console.log(`Scraped ${sailings.length} routes`);
+  return sailings;
 };
 
-const insert = async routes => {
-  const routesResult =
-    routes.map(async route => {
-      const { sailings, ...routePayload } = route;
-      // console.log(JSON.stringify(route, null, 2));
-      const routeResult = await request(uri, upsertRoute, routePayload);
-      console.log(`\nUpserted route info for ${route.route_name}`);
-      // console.log(JSON.stringify(routeResult, null, 2));
+const primaryInsert = async routes => {
+  const routesResult = routes.map(async route => {
+    const { sailings, ...routePayload } = route;
+    const routeResult = await request(uri, upsertRoute, routePayload);
+    console.log(`Upserted route info for ${route.route_name}`);
 
-      const route_id = routeResult.insert_route.returning[0].id;
-      const sailingResults = Object.values(sailings).map(async sailing => {
-        const sailingPayload = { ...sailing, route_id };
-        return request(uri, upsertSailing, {
-          objects: sailingPayload
-        });
-        // console.log(JSON.stringify(res, null, 2));
+    const route_id = routeResult.insert_route.returning[0].id;
+    const sailingResults = Object.values(sailings).map(async sailing => {
+      const sailingPayload = { ...sailing, route_id };
+      return request(uri, upsertSailing, {
+        objects: sailingPayload,
       });
-      
-      
+    });
 
-      console.log(
-        `Upserted ${sailingResults.length} sailings for ${route.route_name}`
-      );
-      
-      return Promise.all(sailingResults)
-    })
-  // console.log(await routesResult)
+    console.log(
+      `Upserted ${sailingResults.length} sailings for ${route.route_name}`
+    );
 
-  console.log(`\nFinished updating ${routesResult.length} routes`);
-  
-  return Promise.all(routesResult)
+    return Promise.all(sailingResults);
+  });
+
+  console.log(`Finished updating ${routesResult.length} routes`);
+  return Promise.all(routesResult);
 };
+
+const secondaryInserts = routesResult =>
+  routesResult.map(async route => {
+    const additionalRouteInfo = await Promise.all(
+      route.map(async sailing => {
+        const {
+          id,
+          scheduled_departure,
+          routeByrouteId,
+        } = sailing.insert_sailing.returning[0];
+        const { departure_term, route_num_str, id: route_id } = routeByrouteId;
+
+        const futureSailing = await getPercentFull({
+          scheduled_departure,
+          departure_term,
+          route_num_str,
+        });
+
+        if (futureSailing) {
+          const {
+            percent_full,
+            parking_full,
+            car_waits,
+            oversize_waits,
+          } = futureSailing;
+          // update sailing with percentage full
+          request(uri, upsertSailingPercent, {
+            id,
+            percent_full,
+          });
+          // return parking and sailing waits so we can update the route table
+          return { id: route_id, parking_full, car_waits, oversize_waits };
+        }
+      })
+    );
+    console.log(
+      `Updated ${additionalRouteInfo.length} sailings with percent full`
+    );
+    const latestRouteInfo = additionalRouteInfo.pop();
+
+    //insert additional route info
+    const res = await request(uri, upsertParkingAndWaits, latestRouteInfo);
+    console.log(
+      `Updated ${
+        res.update_route.returning[0].id
+      } with parking and sailing waits`
+    );
+  });
 
 const main = () => {
-  scrape()
-    .then(routes => insert(routes))
-    .then(routes => routes.map(route =>  route.map(async sailing => {
-     const {id, scheduled_departure, routeByrouteId} = sailing.insert_sailing.returning[0]
-     const {departure_term, route_num_str} = routeByrouteId
-     
-      const res = await getPercentFull({scheduled_departure, departure_term, route_num_str})
-      
-      if (res) {
-        const {percent, parking, carWaits, oversizeWaits} = res
-        const res2 = await request(uri, upsertSailingPercent, {id, percent_full: percent});
-        console.log(res2)
-
-      }
-      
-    }
-      )))
+  scrapeSailingSchedule()
+    .then(routes => primaryInsert(routes))
+    .then(routesResult => secondaryInserts(routesResult))
     .catch(err => {
       throw err;
     });
 };
 
-main()
+// main();
 
 module.exports = { main };
