087b1d7d3e73e1f9b3ac1d53916f123f7b0f25dc
Dean Sallinen <deza604@gmail.com>
2020-07-15 14:21:38 -0700

    connect svelte to json file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,21 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script>
+  /* import {onMount} from 'svelte'; */
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  import data from '../sailings.json';
 
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+  console.log(data[0]);
+  /* onMount(async () => { }); */
+</script>
+<style> </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Ferrytime</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<ul>
+  {#each data as route} 
+    <li>{route.route_name}</li>
+  {/each}
+</ul>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<pre>{JSON.stringify(data, null, 2)}</pre>
