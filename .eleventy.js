2e8c8614b7a6d6f5908713891ed3b8fc03b32213
Dean Sallinen <deza604@gmail.com>
2020-08-05 20:03:42 -0700

    start with fresh snowpack

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const slugify = require('slugify');
+const { parseISO, format } = require('date-fns');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     // Templates:
     'html',
     'njk',
     'md',
+    'liquid',
     // Static Assets:
     'css',
     'jpeg',
@@ -14,6 +18,46 @@ module.exports = function (eleventyConfig) {
     'woff2',
   ]);
   eleventyConfig.addPassthroughCopy('static');
+  eleventyConfig.addLayoutAlias('default', 'layouts/base.liquid');
+  eleventyConfig.addFilter('departureTerminal', (routeName) => {
+    const [departureTerminal, arrivalTerminal] = routeName.split(' to ');
+    return departureTerminal;
+  });
+
+  eleventyConfig.addFilter('arrivalTerminal', (routeName) => {
+    const [departureTerminal, arrivalTerminal] = routeName.split(' to ');
+    return arrivalTerminal;
+  });
+
+  eleventyConfig.addFilter('jsonify', (input) =>
+    JSON.stringify(input, null, 2),
+  );
+
+  eleventyConfig.addFilter('latestStatus', (sailings) => {
+    const { sailing_status } = Object.values(sailings).reduce((acc, cur) =>
+      cur.actual_departure ? cur : acc,
+    );
+    if (
+      !['On Time', 'Cancelled', '', null, undefined].includes(sailing_status)
+    ) {
+      return 'Delayed';
+    } else {
+      return sailing_status;
+    }
+  });
+
+  eleventyConfig.addFilter('formatTime', (time) => {
+    return time ? format(parseISO(time), 'H:mm') : null;
+  });
+
+  eleventyConfig.addFilter('slug', (input) => {
+    const options = {
+      replacement: '-',
+      remove: /[&,+()$~%.'":*?<>{}]/g,
+      lower: true,
+    };
+    return slugify(input, options);
+  });
 
   return {
     dir: {
